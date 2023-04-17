1d60488a2dc090611f276571ea640a3edd4b8f6a
Maria de Fátima <104330898+MariadeFatima624@users.noreply.github.com>
2023-04-16 23:12:46 -0300

    Add files via upload

---
+++ caResistores/caResistores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caResistores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os dados solicitaados no seguinte formato:\n Valor da resistência \n *pressione enter* \n Valor da máxima potência \n *pressione enter*");
+            Console.ReadLine();
+            Resistor r1 = new Resistor("Digite os dados do resisro 1:");
+            Console.WriteLine("\n");
+            Resistor r2 = new Resistor("Digite os dados do resisro 2:");
+            Console.WriteLine("\n");
+            r1.imprimeResistor(r2);
+        }
+    }
+}
---
+++ caResistores/caResistores/Resistor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caResistores
+{
+    internal class Resistor
+    {
+        //Atributos
+        private double resistencia;
+        private double potenciaMax;
+
+        //Métodos
+
+        public Resistor() { }
+
+        public Resistor(double _resistencia, double _potenciaMax)
+        {
+            resistencia = _resistencia;
+            potenciaMax = _potenciaMax;
+        }
+
+        public Resistor (string str)
+        {
+            Console.WriteLine(str);
+            resistencia = Convert.ToDouble(Console.ReadLine());
+            potenciaMax = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public Resistor resistorSerie( Resistor R)
+        {
+            Resistor rSerie = new Resistor();
+
+            rSerie.resistencia = resistencia+R.resistencia;
+
+            if(potenciaMax < R.potenciaMax)
+                rSerie.potenciaMax = potenciaMax;
+           
+            else
+                rSerie.potenciaMax = R.potenciaMax;
+
+            return rSerie;
+
+        }
+
+        public Resistor resistorParalelo(Resistor R)
+        {
+            Resistor rParalelo = new Resistor();
+
+            rParalelo.resistencia = (resistencia * R.resistencia)/(resistencia + R.resistencia);
+
+            if (potenciaMax < R.potenciaMax)
+                rParalelo.potenciaMax = potenciaMax;
+
+            else
+                rParalelo.potenciaMax = R.potenciaMax;
+
+            return rParalelo;
+
+        }
+
+        public void imprimeResistor(Resistor r2)
+        {
+            Console.WriteLine("Dados do resistor r1:");
+            Console.WriteLine("Resistência: " + resistencia);
+            Console.WriteLine("Potência máxima: " + potenciaMax);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dados do resistor r2:");
+            Console.WriteLine("Resistência: " + r2.resistencia);
+            Console.WriteLine("Potência máxima: " + r2.potenciaMax);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dados do resistor equivalente da associação em série de r1 e r2:");
+            Console.WriteLine("Resistência: " + resistorSerie(r2).resistencia);
+            Console.WriteLine("Potência máxima: " + resistorSerie(r2).potenciaMax);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dados do resistor equivalente da associação em paralelo de r1 e r2:");
+            Console.WriteLine("Resistência: " + resistorParalelo(r2).resistencia);
+            Console.WriteLine("Potência máxima: " + resistorParalelo(r2).potenciaMax);
+            Console.ReadLine();
+        }
+
+
+        public double Resistencia { get => resistencia; set => resistencia = value; }
+        public double PotenciaMax { get => potenciaMax; set => potenciaMax = value; }
+    }//Fim da classe resistor
+}
