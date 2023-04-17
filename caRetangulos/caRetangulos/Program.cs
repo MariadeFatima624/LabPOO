2c1601975fa0ae74f99a2831b07caf98368f55f0
Maria de Fátima <104330898+MariadeFatima624@users.noreply.github.com>
2023-04-16 23:13:20 -0300

    Add files via upload

---
+++ caRetangulos/caRetangulos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo(10, 20);
+
+            Console.WriteLine("Dados do retângulo r1:");
+            //Console.ReadLine();
+            r1.Imprime();
+            Console.ReadLine();
+
+            r1.mudaAltura(25);
+            r1.mudaComprimento(50);
+            Console.WriteLine("Novos dados do retângulo r1:");
+            //Console.ReadLine();
+            r1.Imprime();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ caRetangulos/caRetangulos/Retangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulos
+{
+    internal class Retangulo
+    {
+        //Atributos
+        private double altura;
+        private double comprimento;
+
+
+
+        //Métodos
+        public Retangulo() { }
+
+        public Retangulo (double _altura, double _comprimento)
+        {
+            altura = _altura;
+            comprimento = _comprimento;
+        }
+
+        public void mudaAltura(double newAltura)
+        {
+            altura = newAltura;
+        }
+
+        public void mudaComprimento(double newComp)
+        {
+            comprimento = newComp;
+        }
+
+        public double CalcPerimetro()
+        {
+            return (2 * (altura + comprimento));
+        }
+
+        public double CalcArea()
+        {
+            return (altura * comprimento);
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("Comprimento: " + comprimento);
+            Console.WriteLine("Perímetro: " + CalcPerimetro());
+            Console.WriteLine("Área: " + CalcArea());
+        }
+        
+       //getters & setters 
+        public double Altura { get => altura; set => altura = value; }
+        public double Comprimento { get => comprimento; set => comprimento = value; }
+
+    }//Fim da classe Retângulo
+}
