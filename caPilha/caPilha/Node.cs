ec70015abe05ece0ed52dc5e75941d72521c2529
Maria de Fátima <104330898+MariadeFatima624@users.noreply.github.com>
2023-06-04 23:59:33 -0300

    Add files via upload

---
+++ caPilha/caPilha/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caPilhas
+{
+    internal class Node
+    {//Atributos
+        private string info;
+        protected string* next;
+        
+     //Métodos
+        public Node() { }
+
+        public Node(string _info)
+        {
+            this.info = _info;
+            this.next = null;
+        }
+        
+
+     //getters & setters
+        public string Info { get => info; set => info = value; }
+        protected string Next { get => next; set => next = value; }
+    }
+}
---
+++ caPilha/caPilha/Pilha.cs
@@ -0,0 +1,70 @@
+﻿using caPilhas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caPilhas
+{
+    internal class Pilha:Node 
+    {
+        
+        //Atributos
+         Node top;
+        //Métodos
+        public Pilha() : base() { }
+
+        public Pilha(string _info):base( _info)
+        {
+            this.top = null;
+        }
+
+        public void push(string _info)
+        {
+            Node nAux = new Node(_info);
+            nAux.next = this.top;
+            this.top = nAux;
+        }
+
+        public bool isempty()
+        {
+            return this.top == null;
+        }
+
+        public string pop()
+        {
+            if (!isempty())
+            {
+                Node nAuxPop = this.top;
+                this.top = nAuxPop.next;
+                return nAuxPop.Info;
+            }
+            else
+            {
+                Console.WriteLine("Pilha vazia!");
+                Console.ReadLine();
+                return null;
+            }
+        }
+
+        public string peek()
+        {
+            if (!isempty())
+            {
+                return this.top.Info;
+            }
+            else
+            {
+                Console.WriteLine("Pilha vazia!");
+                Console.ReadLine();
+                return null;
+            }
+        }
+
+
+
+     //getters & setters
+        public Node Top { get => top; set => top = value; }
+    }
+}
